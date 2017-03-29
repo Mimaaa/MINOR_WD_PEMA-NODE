23581d504d29dd5c4fc6967121020c91c01434d8
Mirza van Meerwijk <mirzavanmeerwijk@gmail.com>
2017-03-29 14:25:47 +0200

    added API

--- app.js
+++ app.js
@@ -7,16 +7,26 @@ var bodyParser = require ('body-parser');
 var app = express();
 
 var api_key = process.env.API_KEY;
+var api_url = process.env.API_URL;
 
 app.use(express.static('public'));
 app.use(require('body-parser').urlencoded({
-  extender: true}));
+  extended: true}));
 
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
 app.get('/', function (req, res) {
   res.render('home');
+});
+
+app.post('/', function(req, res) {
+  var query = req.body.query;
+  var searchUrl = api_url + '?query=' + query + '&api-key=' + api_key;
+  request(searchUrl, function (error, response, body) {
+    res.locals.data = JSON.parse(body);
+    res.render('home');
+  });
 });
 
 app.listen(3000);
