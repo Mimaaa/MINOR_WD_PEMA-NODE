0f750096a3b5e74ee2d9cdd93157b62c9ad01415
Mirza van Meerwijk <mirzavanmeerwijk@gmail.com>
2017-03-31 12:31:36 +0200

    addded code comments

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+// Set all the requires
 var express = require('express');
 var compression = require('compression');
 var exphbs = require ('express-handlebars');
@@ -5,27 +6,36 @@ var dotenv = require ('dotenv').config();
 var request = require ('request');
 var bodyParser = require ('body-parser');
 
+// Use express
 var app = express();
 
+// Enabling gZip compression and tell the module to compress all files
 app.use(compression({
     threshold: 0,
     filter: () => true,
 }));
 
+// Get the key & url from the .env file
 var api_key = process.env.API_KEY;
 var api_url = process.env.API_URL;
 
+// Tell the app that it can find the static files in the public dir
 app.use(express.static('public'));
+
+// Enabling body-parser
 app.use(require('body-parser').urlencoded({
   extended: true}));
 
+// Tell the app to use Handlebars, the default template can be found in the main file
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
+// Set the home route
 app.get('/', function (req, res) {
   res.render('home');
 });
 
+// Getting data from the API with a request where the results are rendered on the homepage based on the query
 app.post('/', function(req, res) {
   var searchUrl = api_url + '/search/movie' + '?api_key=' + api_key + '&query=' + req.body.query;
   request(searchUrl, function (error, response, body) {
@@ -34,6 +44,7 @@ app.post('/', function(req, res) {
   });
 });
 
+// Getting data from the API with a request where the results are rendered on the movie page based on the ID that is requested
 app.get('/movie/:id', function (req, res) {
   var movieUrl = api_url + '/movie/' + req.params.id + '?api_key=' + api_key;
   request(movieUrl, function (error, response, body) {
@@ -42,4 +53,5 @@ app.get('/movie/:id', function (req, res) {
   });
 });
 
+// Run server
 app.listen(3000);
